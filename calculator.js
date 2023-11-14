54d64be9b2b4c5a24aed460990e5a8e9b80e966f
Maria João <142997408+mariajoaomotaa@users.noreply.github.com>
2023-11-15 00:03:43 +0100

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -3,18 +3,39 @@ document.addEventListener("DOMContentLoaded", function () {
     let currentInput = document.querySelector('.currentInput');
     let buttons = document.querySelectorAll('button');
     let clear = document.querySelector('#clear');
+    let sim = document.querySelector('#sim');
+    let porcent = document.querySelector('#porcent');
     let evaluate = document.querySelector('#evaluate');
     let currentValue = [''];
 
     clear.addEventListener("click", () => {
         currentValue = [''];
+        answer.innerHTML = '0';
+        updateDisplay();
+    });
+
+    sim.addEventListener ("click", () => {
+        let str = currentValue.join('');
+        let currentValueNumber = eval(str);
+        let simetric = currentValueNumber * -1;
+        currentValue = [simetric.toString()];
+        updateDisplay();
+    });
+
+    porcent.addEventListener ("click", () => {
+        currentValue = [currentValue+'%'];
         updateDisplay();
     });
 
     buttons.forEach((btn) => {
         btn.addEventListener("click", () => {
-            if (btn.classList.contains('number')) {
+            if (btn.classList.contains('number') || btn.value === '.') {
                 currentValue.push(btn.value);
+                updateDisplay();
+            }
+
+            if (btn.classList.contains('op')) {
+                currentValue.push(' ' + btn.value + ' ');
                 updateDisplay();
             }
 
